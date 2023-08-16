94180b3e0990fa54db694ed4a26da975dbd9ecd8
Carlo Askander <carloaskander@live.se>
2023-08-16 13:12:42 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+import Home from './pages/Home'
+import About from './pages/About'
+
+function App() {
+    return <div className="App">
+        
+    </div>
+}
+
+export default App
